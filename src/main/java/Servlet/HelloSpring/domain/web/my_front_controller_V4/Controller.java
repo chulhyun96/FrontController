92908cbc76dc1a0debcd6409271b18598b641d62
박철현 <tghy4@naver.com>
2024-02-26 11:40:40 +0900

    save

--- src/main/java/Servlet/HelloSpring/domain/web/my_front_controller_V4/Controller.java
+++
@@ -1,7 +0,0 @@
-package Servlet.HelloSpring.domain.web.my_front_controller_V4;
-
-import java.util.Map;
-
-public interface Controller {
-    String service (Map<String,String> paraMap, Map<String, Object> model);
-}
